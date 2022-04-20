6e504951f824b62af9e34a7aaed84d5353b84e7d
Ellis Ballard <eab4@njit.edu>
2022-04-20 14:20:14 -0400

    changes to click test file

--- tests/click_test.py
+++ tests/click_test.py
@@ -1,5 +1,5 @@
 import os
-
+from pathlib import Path
 from click.testing import CliRunner
 
 from app import create_log_folder
@@ -10,8 +10,14 @@ runner = CliRunner()
 def test_add():
     response = runner.invoke(create_log_folder)
     assert response.exit_code == 0
-    root = os.path.dirname(os.path.abspath(__file__))
+    file_name = Path(__file__)
+    test_dir = file_name.parent
+    proj_dir = test_dir.parent
+    app_dir = proj_dir / "app"
+    log_dir = app_dir / "logs"
+    #root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '../logs')
+    #logdir = os.path.join(root, '../logs')
     # make a directory if it doesn't exist
-    assert os.path.exists(logdir) == True
+    assert log_dir.exists()
+    #assert os.path.exists(log_dir) == True
