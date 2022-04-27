650129608eb4212cc9454b74ac75ee26ee29fedb
Ellis Ballard <eab4@njit.edu>
2022-04-26 20:49:33 -0400

    Fixed pytest with logging debugs and requests

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -7,8 +7,7 @@ def test_debug():
     file_name = Path(__file__)
     test_dir = file_name.parent
     proj_dir = test_dir.parent
-    app_dir = proj_dir / "app"
-    log_dir = app_dir / "logs"
+    log_dir = proj_dir / "logs"
     debug_file = log_dir / "errors.log"
     assert debug_file.exists()
 
@@ -17,8 +16,7 @@ def test_request():
     file_name = Path(__file__)
     test_dir = file_name.parent
     proj_dir = test_dir.parent
-    app_dir = proj_dir / "app"
-    log_dir = app_dir / "logs"
+    log_dir = proj_dir / "logs"
     request_file = log_dir / "request.log"
     assert request_file.exists()
 
